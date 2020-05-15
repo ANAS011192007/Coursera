5d2a2cd8106cc5ee6ead4fdea2a1ed1a70d3a788
ANAS011192007 <52394372+ANAS011192007@users.noreply.github.com>
2020-05-16 01:00:12 +0600

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,6 +1,6 @@
 (function (window) {
 	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-	for (var i=0;i<names.length;i++) {
+	for (var i in names) {
 	var firstLetter = ((names[i]).charAt(0)).toLowerCase();
 	 if (firstLetter==j) {
     window.byeSpeaker.(names[i]);
